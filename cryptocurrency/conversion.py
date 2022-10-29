65af27f10ff1d51166f19ae8169f729fd7a61799
Samuel Duclos <nomfullcreatif@gmail.com>
2022-10-29 15:57:27 -0400

    Converted.

--- cryptocurrency/conversion.py
+++ cryptocurrency/conversion.py
@@ -7,6 +7,8 @@
 # Description: Binance asset conversion.
 
 # Library imports.
+from cryptocurrency.ohlcvs import named_pairs_to_df
+from tqdm import tqdm
 import pandas as pd
 
 def get_assets_from_pair(pair, exchange_info):
@@ -41,7 +43,12 @@ def get_connected_assets(asset, exchange_info):
     connected_quote_assets = exchange_info['quote_asset'] == asset
     connected_quote_assets = exchange_info[connected_quote_assets]
     connected_quote_assets = connected_quote_assets['base_asset'].tolist()
-    return sorted(list(set(connected_base_assets + connected_quote_assets)))
+    connected_assets = sorted(list(set(connected_base_assets + connected_quote_assets)))
+    if 'BNB' in connected_assets and 'BTC' in connected_assets:
+        BNB, BTC = connected_assets.index('BNB'), connected_assets.index('BTC')
+        connected_assets[BNB], connected_assets[BTC] = \
+            connected_assets[BTC], connected_assets[BNB]
+    return connected_assets
 
 def get_shortest_pair_path_between_assets(from_asset, to_asset, exchange_info):
     def get_shortest_path_between_assets():
@@ -103,6 +110,7 @@ def convert_price(size, from_asset, to_asset, conversion_table, exchange_info):
         from_asset = to_asset
     return size
 
+'''
 def convert_ohlcv(from_asset, to_asset, conversion_table, exchange_info):
     shortest_path = get_shortest_pair_path_between_assets(from_asset=from_asset, 
                                                           to_asset=to_asset, 
@@ -111,11 +119,9 @@ def convert_ohlcv(from_asset, to_asset, conversion_table, exchange_info):
     to_asset = quote_asset if from_asset == base_asset else base_asset
     pair = base_asset + quote_asset
     size = conversion_table[pair].copy()
-    size = size.astype(float)
     size['quote_volume'] = size['quote_volume'] / size['close']
     for (base_asset, quote_asset) in shortest_path[1:]:
         to_asset = quote_asset if from_asset == base_asset else base_asset
-        size = size.astype(float)
         pair = base_asset + quote_asset
         connection = conversion_table[pair]
         if base_asset == from_asset:
@@ -123,41 +129,31 @@ def convert_ohlcv(from_asset, to_asset, conversion_table, exchange_info):
             size['high'] = size['close'] * connection['high']
             size['low'] = size['close'] * connection['low']
             size['close'] = size['close'] * connection['close']
-            size['base_volume'] = size['close'] * connection['base_volume']
-            size['quote_volume'] = size['close'] * connection['quote_volume']
         else:
             size['open'] = size['open'] / connection['open']
             size['high'] = size['close'] / connection['high']
             size['low'] = size['close'] / connection['low']
             size['close'] = size['close'] / connection['close']
-            size['base_volume'] = size['close'] / connection['base_volume']
-            size['quote_volume'] = size['close'] / connection['quote_volume']
         from_asset = to_asset
-    return size.rename(columns={'open': 'open_' + to_asset, 
-                                'high': 'high_' + to_asset, 
-                                'low': 'low_' + to_asset, 
-                                'close': 'close_' + to_asset, 
-                                'base_volume': 'base_volume_' + to_asset, 
-                                'quote_volume': 'quote_volume_' + to_asset})
+    size['base_volume'] = size['close'] * size['base_volume']
+    size['quote_volume'] = size['close'] * size['quote_volume']
+    size.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+    size.loc[:, ['base_volume', 'quote_volume']] = \
+        size.loc[:, ['base_volume', 'quote_volume']].fillna(0)
+    return size.fillna(method='pad')
 
-'''
-def convert_ohlcvs(from_asset, to_asset, conversion_table, exchange_info):
-    results = conversion_table.copy()
-    
-    symbols = conversion_table.get_level_values(0).tolist()
-    assets = [get_base_asset_from_pair(symbol, exchange_info) for symbol in symbols]
-    for symbol in symbols:
+def convert_ohlcvs(to_asset, conversion_table, exchange_info):
+    def convert_ohlcv_prices(from_asset, to_asset, conversion_table, exchange_info):
         shortest_path = get_shortest_pair_path_between_assets(from_asset=from_asset, 
                                                               to_asset=to_asset, 
                                                               exchange_info=exchange_info)
         base_asset, quote_asset = shortest_path[0]
         to_asset = quote_asset if from_asset == base_asset else base_asset
-        size = size.astype(float)
         pair = base_asset + quote_asset
         size = conversion_table[pair].copy()
+        size['quote_volume'] = size['quote_volume'] / size['close']
         for (base_asset, quote_asset) in shortest_path[1:]:
             to_asset = quote_asset if from_asset == base_asset else base_asset
-            size = size.astype(float)
             pair = base_asset + quote_asset
             connection = conversion_table[pair]
             if base_asset == from_asset:
@@ -165,16 +161,41 @@ def convert_ohlcvs(from_asset, to_asset, conversion_table, exchange_info):
                 size['high'] = size['close'] * connection['high']
                 size['low'] = size['close'] * connection['low']
                 size['close'] = size['close'] * connection['close']
-                size['volume'] = size['close'] * connection['volume']
             else:
                 size['open'] = size['open'] / connection['open']
                 size['high'] = size['close'] / connection['high']
                 size['low'] = size['close'] / connection['low']
                 size['close'] = size['close'] / connection['close']
-                size['volume'] = size['close'] / connection['volume']
             from_asset = to_asset
-        results[symbol] = size
-    return results
+        return size
+    def convert_ohlcv_volumes(symbol, conversion_table, exchange_info):
+        from_asset = get_base_asset_from_pair(symbol, exchange_info)
+        size = conversion_table[symbol].copy()
+        size['base_volume'] = size['close'] * size['base_volume']
+        size['quote_volume'] = size['close'] * size['quote_volume']
+        size.replace([float('inf'), float('-inf')], float('nan'), inplace=True)
+        size.loc[:, ['base_volume', 'quote_volume']] = \
+            size.loc[:, ['base_volume', 'quote_volume']].fillna(0)
+        return size.fillna(method='pad')
+    def convert_ohlcv_prices_helper(symbol):
+        from_asset = get_base_asset_from_pair(symbol, exchange_info)
+        if from_asset == to_asset:
+            size = conversion_table[symbol].copy()
+            size['quote_volume'] = size['quote_volume'] / size['close']
+        else:
+            size = convert_ohlcv_prices(from_asset, to_asset, 
+                                        conversion_table, exchange_info)
+        return size
+    conversion_table = conversion_table.astype(float).copy()
+    symbols = conversion_table.columns.get_level_values(0).unique().tolist()
+    conversion_table = [convert_ohlcv_prices_helper(symbol) 
+                        for symbol in tqdm(symbols, unit=' pair conversion')]
+    conversion_table = named_pairs_to_df(symbols, conversion_table)
+    conversion_table = [convert_ohlcv_volumes(symbol, conversion_table, 
+                                              exchange_info) 
+                        for symbol in tqdm(symbols, unit=' pair conversion')]
+    conversion_table = named_pairs_to_df(symbols, conversion_table)
+    return conversion_table
 '''
 
 def select_asset_with_biggest_wallet(client, conversion_table):
--- cryptocurrency/ohlcvs.py
+++ cryptocurrency/ohlcvs.py
@@ -10,16 +10,17 @@ from cryptocurrency.ohlcv import download_pair
 from tqdm import tqdm
 import pandas as pd
 
+def named_pairs_to_df(assets, pairs):
+    df = pd.DataFrame()
+    column_names = pairs[0].columns.tolist()
+    for (asset, pair) in tqdm(zip(assets, pairs), unit=' named pair'):
+        columns = [(asset, column) for column in column_names]
+        pair.columns = pd.MultiIndex.from_tuples(columns, names=['symbol', 'pair'])
+        df = pd.concat([df, pair], axis='columns')
+    return df
+
 def download_pairs(client, assets, interval='1m', period=2880, second_period=None):
     def download_pairs_helper(period=2880):
-        def named_pairs_to_df(assets, pairs):
-            df = pd.DataFrame()
-            column_names = pairs[0].columns.tolist()
-            for (asset, pair) in tqdm(zip(assets, pairs), unit=' named pair'):
-                columns = [(asset, column) for column in column_names]
-                pair.columns = pd.MultiIndex.from_tuples(columns, names=['symbol', 'pair'])
-                df = pd.concat([df, pair], axis='columns')
-            return df
         pairs = [download_pair(client=client, symbol=symbol, interval=interval, 
                                period=period) for symbol in tqdm(assets, unit=' pair')]
         pairs = named_pairs_to_df(assets, pairs)

f1f0c9942daf71bb6aa3bb752c1b0c5561be80b4
Samuel Duclos <nomfullcreatif@gmail.com>
2022-11-16 12:58:49 -0500

    Grouped interval iterations in single loops.

--- crypto_logger.py
+++ crypto_logger.py
@@ -20,7 +20,7 @@ def start_loggers():
     """Main logger loop."""
     print('Starting crypto logger.')
     crypto_logger_input_15s = Crypto_logger_input(delay=4.7, interval='15s', buffer_size=3000, 
-                                                  price_percent=1.0, volume_percent=0.0, 
+                                                  price_percent=5.0, volume_percent=0.0, 
                                                   as_pair=False, append=True, roll=60, log=True)
     crypto_logger_output_15s = Crypto_logger_output(delay=12, 
                                                     interval_input='15s', 
@@ -59,8 +59,8 @@ def start_loggers():
                                                    interval='1d', 
                                                    buffer_size=60, 
                                                    input_log_name='output', 
-                                                   append=True, 
-                                                   roll=12, 
+                                                   append=False, 
+                                                   roll=1000, 
                                                    log=True)
     crypto_loggers = [
         crypto_logger_input_15s, 
@@ -74,14 +74,14 @@ def start_loggers():
         crypto_logger.init()
     try:
         while True:
-            t1 = time.time()
+            #t1 = time.time()
             for crypto_logger in crypto_loggers:
                 dataset = crypto_logger.concat_next()
                 crypto_logger.process_next(dataset)
                 crypto_logger.log_next()
-            t2 = time.time()
-            print('Time spent for one loop:', t2 - t1)
-            time.sleep(crypto_loggers[0].delay)
+            #t2 = time.time()
+            #print('Time spent for one loop:', t2 - t1)
+            #time.sleep(crypto_loggers[0].delay)
     except (KeyboardInterrupt, SystemExit):
         print('Saving latest complete dataset...')
         crypto_logger.process_next(dataset)

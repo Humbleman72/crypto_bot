1210710bd4c7e0c65b2ed6b809401888c24cee8a
Samuel Duclos <nomfullcreatif@gmail.com>
2022-12-19 13:34:40 -0500

    One iteration is ~10 seconds.

--- cryptocurrency/conversion.py
+++ cryptocurrency/conversion.py
@@ -46,32 +46,43 @@ def get_quote_asset_from_pair(pair, exchange_info):
 
 def get_connected_assets(asset, exchange_info, priority='accuracy'):
     def reorder(connected_assets, priority):
-        prioritized = [asset for asset in priority if asset in connected_assets]
+        prioritized = \
+            [asset for asset in priority if asset in connected_assets]
         order = {asset: i for i, asset in enumerate(prioritized)}
-        connected_assets_items = [asset for asset in connected_assets if asset in order]
+        connected_assets_items = \
+            [asset for asset in connected_assets if asset in order]
         connected_assets_items.sort(key=order.get)
         connected_assets_iter = iter(connected_assets_items)
         return [next(connected_assets_iter) if asset in order 
                 else asset for asset in connected_assets]
     if priority == 'accuracy':
-        priority = ['USDT', 'BTC', 'BUSD', 'ETH', 'BNB', 'AUD']
+        priority = ['USDT', 'BTC', 'BUSD', 'ETH', 'BNB']
     elif priority == 'fees':
-        priority = ['BUSD', 'BTC', 'BNB', 'ETH', 'USDT', 'AUD']
+        priority = ['BUSD', 'BTC', 'BNB', 'ETH', 'USDT']
     elif priority == 'wallet':
-        priority = ['BTC', 'ETH', 'BUSD', 'BNB', 'USDT', 'AUD']
+        priority = ['BTC', 'ETH', 'BUSD', 'BNB', 'USDT']
+    priority += ['BRL', 'AUD']
     connected_base_assets = exchange_info['quote_asset'] == asset
     connected_base_assets = exchange_info[connected_base_assets]
-    connected_base_assets = connected_base_assets['base_asset'].tolist()
+    connected_base_assets = connected_base_assets['base_asset']
+    connected_base_assets = connected_base_assets.tolist()
     connected_quote_assets = exchange_info['base_asset'] == asset
     connected_quote_assets = exchange_info[connected_quote_assets]
-    connected_quote_assets = connected_quote_assets['quote_asset'].tolist()
-    connected_assets = list(set(connected_base_assets + connected_quote_assets))
+    connected_quote_assets = connected_quote_assets['quote_asset']
+    connected_quote_assets = connected_quote_assets.tolist()
+    connected_assets = \
+        list(set(connected_base_assets + connected_quote_assets))
     return reorder(connected_assets, priority)
 
-def select_pair_with_highest_quote_volume_from_base_asset(base_asset, conversion_table, exchange_info):
-    connected_pairs = exchange_info[exchange_info['base_asset'] == base_asset]['symbol']
-    connected_pairs = conversion_table[conversion_table['symbol'].isin(connected_pairs)]
-    connected_pairs = connected_pairs.sort_values(by='rolling_quote_volume', ascending=False)
+def select_pair_with_highest_quote_volume_from_base_asset(base_asset, 
+                                                          conversion_table, 
+                                                          exchange_info):
+    connected_pairs = exchange_info[exchange_info['base_asset'] == base_asset]
+    connected_pairs = connected_pairs['symbol']
+    connected_pairs = \
+        conversion_table[conversion_table['symbol'].isin(connected_pairs)]
+    connected_pairs = connected_pairs.sort_values(by='rolling_quote_volume', 
+                                                  ascending=False)
     return connected_pairs['symbol'].iat[0]
 
 def get_shortest_pair_path_between_assets(from_asset, to_asset, exchange_info, 
@@ -85,7 +96,8 @@ def get_shortest_pair_path_between_assets(from_asset, to_asset, exchange_info,
         while path_index < len(path_list):
             current_path = path_list[path_index]
             last_node = current_path[-1]
-            next_nodes = get_connected_assets(last_node, exchange_info=exchange_info, 
+            next_nodes = get_connected_assets(last_node, 
+                                              exchange_info=exchange_info, 
                                               priority=priority)
             if to_asset in next_nodes:
                 current_path.append(to_asset)
@@ -103,9 +115,12 @@ def get_shortest_pair_path_between_assets(from_asset, to_asset, exchange_info,
         from_asset_is_quote = exchange_info['quote_asset'] == from_asset
         to_asset_is_base = exchange_info['base_asset'] == to_asset
         to_asset_is_quote = exchange_info['quote_asset'] == to_asset
-        from_asset_is_base_and_to_asset_is_quote = from_asset_is_base & to_asset_is_quote
-        from_asset_is_quote_and_to_asset_is_base = from_asset_is_quote & to_asset_is_base
-        pair = from_asset_is_base_and_to_asset_is_quote | from_asset_is_quote_and_to_asset_is_base
+        from_asset_is_base_and_to_asset_is_quote = \
+            from_asset_is_base & to_asset_is_quote
+        from_asset_is_quote_and_to_asset_is_base = \
+            from_asset_is_quote & to_asset_is_base
+        pair = from_asset_is_base_and_to_asset_is_quote
+        pair |= from_asset_is_quote_and_to_asset_is_base
         if pair.any():
             pair = exchange_info[pair]
             base_asset = pair['base_asset'].iat[0]
@@ -147,10 +162,9 @@ def convert_price(size, from_asset, to_asset, conversion_table, exchange_info,
     if from_asset != to_asset:
         size = float(size)
         if shortest_path is None:
-            shortest_path = get_shortest_pair_path_between_assets(from_asset=from_asset, 
-                                                                  to_asset=to_asset, 
-                                                                  exchange_info=exchange_info, 
-                                                                  priority=priority)
+            shortest_path = get_shortest_pair_path_between_assets(
+                from_asset=from_asset, to_asset=to_asset, 
+                exchange_info=exchange_info, priority=priority)
         for (base_asset, quote_asset) in shortest_path:
             to_asset = quote_asset if from_asset == base_asset else base_asset
             pair = base_asset + quote_asset

9ee8f6280ef811a4cd1c170c22b63ce1c1c73754
Samuel Duclos <nomfullcreatif@gmail.com>
2022-12-23 17:24:28 -0500

    Writing aarch64-portable install procedure.

--- install/install_conda_crypto_bot_environment.sh
+++ install/install_conda_crypto_bot_environment.sh
@@ -7,15 +7,45 @@
 # Description: Installs all packages required for the crypto_bot.
 
 CONDA_ENV_NAME="crypto_bot"
-PYTHON_VERSION="3"
 
 # Automagic.
-/opt/conda/bin/conda create --yes --name ${CONDA_ENV_NAME} --channel conda-forge gcc gxx_linux-64 gcc_linux-64 cython numexpr bottleneck pandas numpy scipy matplotlib seaborn tqdm setuptools-git mosquitto paramiko ta-lib jupyterlab ipywidgets widgetsnbextension typeguard flask pip python=${PYTHON_VERSION} && \
+/opt/conda/bin/conda create --yes --name ${CONDA_ENV_NAME} --channel conda-forge gcc \
+                                                                                 gxx_linux-64 \
+                                                                                 cython \
+                                                                                 numpy \
+                                                                                 bottleneck \
+                                                                                 numexpr \
+                                                                                 pandas \
+                                                                                 scipy \
+                                                                                 matplotlib \
+                                                                                 seaborn \
+                                                                                 setuptools-git \
+                                                                                 tqdm \
+                                                                                 mosquitto \
+                                                                                 paramiko \
+                                                                                 regex \
+                                                                                 ta \
+                                                                                 ta-lib \
+                                                                                 ipywidgets \
+                                                                                 jupyterlab \
+                                                                                 widgetsnbextension \
+                                                                                 typeguard \
+                                                                                 typing_extensions \
+                                                                                 click \
+                                                                                 dateparser \
+                                                                                 beautifulsoup4 \
+                                                                                 flask-socketio \
+                                                                                 requests \
+                                                                                 websockets \
+                                                                                 python-binance \
+                                                                                 pip \
+                                                                                 python=3 && \
 /opt/conda/bin/conda init bash && \
 source /opt/conda/bin/activate ${CONDA_ENV_NAME} && \
 if [[ "$(echo $PS1 | cut -d' ' -f1 | tr -d '()')" == "${CONDA_ENV_NAME}" ]]
 then
-    yes | pip install --upgrade --no-cache-dir numpy python-binance ta 'typing_extensions==3.10.0.2' git+https://github.com/twopirllc/pandas-ta
+    #yes | pip install --upgrade --no-cache-dir numpy git+https://github.com/twopirllc/pandas-ta
+    yes | pip install --upgrade --no-cache-dir git+https://github.com/twopirllc/pandas-ta
 else
     echo "Could not source activate ${CONDA_ENV_NAME}..."
 fi
--- install/install_conda_crypto_logger_environment.sh
+++ install/install_conda_crypto_logger_environment.sh
@@ -7,15 +7,30 @@
 # Description: Installs all packages required for the crypto_logger.
 
 CONDA_ENV_NAME="crypto_logger"
-PYTHON_VERSION="3"
 
 # Automagic.
-/opt/conda/bin/conda create --yes --name ${CONDA_ENV_NAME} --channel conda-forge gcc gxx_linux-64 gcc_linux-64 cython numexpr bottleneck numpy pandas scipy matplotlib tqdm ta-lib pip python=${PYTHON_VERSION} && \
+/opt/conda/bin/conda create --yes --name ${CONDA_ENV_NAME} --channel conda-forge gcc \
+                                                                                 gxx_linux-64 \
+                                                                                 cython \
+                                                                                 bottleneck \
+                                                                                 numexpr \
+                                                                                 numpy \
+                                                                                 pandas \
+                                                                                 matplotlib \
+                                                                                 scipy \
+                                                                                 ta \
+                                                                                 ta-lib \
+                                                                                 tqdm \
+                                                                                 python-binance \
+                                                                                 typing_extensions \
+                                                                                 pip \
+                                                                                 python=3 && \
 /opt/conda/bin/conda init bash && \
 source /opt/conda/bin/activate ${CONDA_ENV_NAME} && \
 if [[ "$(echo $PS1 | cut -d' ' -f1 | tr -d '()')" == "${CONDA_ENV_NAME}" ]]
 then
-    yes | pip install --upgrade --no-cache-dir numpy python-binance ta 'typing_extensions==3.10.0.2' git+https://github.com/twopirllc/pandas-ta
+    #yes | pip install --upgrade --no-cache-dir numpy git+https://github.com/twopirllc/pandas-ta
+    yes | pip install --upgrade --no-cache-dir git+https://github.com/twopirllc/pandas-ta
 else
     echo "Could not source activate ${CONDA_ENV_NAME}..."
 fi

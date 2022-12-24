ef5d44aa0b7a9cb70f41963c84b210bad0091915
Samuel Duclos <nomfullcreatif@gmail.com>
2022-12-24 00:14:33 -0500

    Reviewed install procedures.

--- install/install_micromamba_crypto_logger_environment.sh
+++ install/install_micromamba_crypto_logger_environment.sh
@@ -1,28 +1,35 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 
 # File:        install/install_micromamba_crypto_logger_environment.sh
 # By:          Samuel Duclos
 # For:         Myself
-# Usage:       bash install/install_micromamba_crypto_logger_environment.sh
+# Usage:       source install/install_micromamba_crypto_logger_environment.sh
 # Description: Installs all packages required for the crypto_logger.
 
 MICROMAMBA_ENV_NAME="crypto_logger"
 
 # Automagic.
-/opt/micromamba/bin/micromamba create --yes --name ${MICROMAMBA_ENV_NAME} gcc \
-                                                                          gxx_linux-64 \
-                                                                          cython \
-                                                                          bottleneck \
-                                                                          numexpr \
-                                                                          numpy \
-                                                                          pandas \
-                                                                          matplotlib \
-                                                                          scipy \
-                                                                          ta \
-                                                                          pandas-ta \
-                                                                          ta-lib \
-                                                                          tqdm \
-                                                                          python-binance \
-                                                                          typing_extensions \
-                                                                          pip \
-                                                                          python=3
+micromamba create --yes --name ${MICROMAMBA_ENV_NAME} gcc \
+                                                      gxx_linux-64 \
+                                                      cython \
+                                                      bottleneck \
+                                                      numexpr \
+                                                      numpy \
+                                                      pandas \
+                                                      matplotlib \
+                                                      scipy \
+                                                      ta \
+                                                      ta-lib \
+                                                      tqdm \
+                                                      python-binance \
+                                                      typing_extensions \
+                                                      pip \
+                                                      python=3 && \
+micromamba activate ${MICROMAMBA_ENV_NAME} && \
+if [[ "$(echo $PS1 | cut -d' ' -f1 | tr -d '()')" == "${MICROMAMBA_ENV_NAME}" ]]
+then
+    yes | pip install --upgrade --no-cache-dir git+https://github.com/twopirllc/pandas-ta
+else
+    echo "Could not source activate ${MICROMAMBA_ENV_NAME}..."
+fi && \
+micromamba deactivate
